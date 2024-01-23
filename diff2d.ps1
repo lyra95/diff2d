0e13d866bef696b0f20d10625328952f61942fc8
lyra95 <lyra95@shiftup.co.kr>
2024-01-23 20:36:36 +0900

    GUI 프로그래밍 in Powershell

--- diff2d.ps1
+++ diff2d.ps1
@@ -1,12 +1,14 @@
 param(
-    [string]$sqliteFilePath1,
-    [string]$sqliteFilePath2
+  [string]$sqliteFilePath1,
+  [string]$sqliteFilePath2
 )
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
 $ErrorActionPreference = "Stop"
 
 if (-not $sqliteFilePath1) {
-    Exit 1
+  Exit 1
 }
 
 if (-not $sqliteFilePath2) {
@@ -14,26 +16,46 @@ if (-not $sqliteFilePath2) {
 }
 
 if (-not (Test-Path $sqliteFilePath1 -PathType Leaf)) {
-    Write-Host "The specified file does not exist: $sqliteFilePath1"
-    Exit 1
-}
-
-if (-not (Test-Path $sqliteFilePath2 -PathType Leaf)) {
-  Write-Host "The specified file does not exist: $sqliteFilePath2"
+  [System.Windows.Forms.MessageBox]::Show("The specified file does not exist: $sqliteFilePath1", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
   Exit 1
 }
 
-$filePath = C:\Users\SHIFTUP\bin\diff2d.exe $sqliteFilePath1 $sqliteFilePath2 2> C:\Users\SHIFTUP\bin\diff2d.log
 
-$IS_SUCCESS = $LASTEXITCODE
-
-# Check the exit code of the program
-if (! $IS_SUCCESS -eq 0) {
-  Write-Host "The program exited with code $IS_SUCCESS"
-  Exit $IS_SUCCESS
+if (-not (Test-Path $sqliteFilePath2 -PathType Leaf)) {
+  [System.Windows.Forms.MessageBox]::Show("The specified file does not exist: $sqliteFilePath2", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+  Exit 1
 }
 
-Write-Host $filePath
+$job = Start-Job -ScriptBlock { .\diff2d.exe $args[0] $args[1] 2> .\diff2d.log } -ArgumentList $sqliteFilePath1, $sqliteFilePath2
+
+# open a progress bar window
+$progressBar = New-Object System.Windows.Forms.Form
+$progressBar.Text = "Please wait..."
+$progressBar.Size = New-Object System.Drawing.Size(400,200)
+$progressBar.FormBorderStyle = 'Fixed3D'
+$progressBar.MaximizeBox = $false
+$progressBar.MinimizeBox = $false
+$progressBar.StartPosition = "CenterScreen"
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.AutoSize = $true
+$textBox.Size = New-Object System.Drawing.Size(200, 200)
+$textBox.Multiline = $true
+$textBox.Text = "1mb 당 1초 정도 소요됩니다."
+$progressBar.Controls.Add($textBox)
+
+$progressBar.Show()
+Wait-Job -Job $job
+$progressBar.Close()
+
+# if job failed, show error message
+if ($job.State -eq "Failed") {
+  $errorMessage = $job.JobStateInfo.Reason.Message
+  [System.Windows.Forms.MessageBox]::Show($errorMessage, "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+  Exit 1
+}
+
+# put job's output into a variable
+$filePath = Receive-Job -Job $job
 
 # Create a new Excel application object
 $excel = New-Object -ComObject Excel.Application
@@ -47,6 +69,3 @@ $workbook = $excel.Workbooks.Open($filePath)
 # Cleanup - Release the Excel COM objects
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($workbook) | Out-Null
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
-
-# Display a message indicating success
-Write-Host "Excel file opened successfully: $filePath"
